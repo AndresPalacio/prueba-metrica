a0bf6e3f664acdeef94ef1440cb801ae2b513683
Jeisson Andres Palacio Velasquez <jeapalac@bancolombia.com.co>
2020-07-07 02:43:02 -0500

    prueba dilema

--- CustomMetric.py
+++ CustomMetric.py
@@ -21,13 +21,14 @@ def appendMetrics(CurrentMetrics, Dimensions, Name, Unit, Value):
 
 def memProcessChromeDrive():
     return len([p.info['pid'] for p in psutil.process_iter(attrs=['pid','name','create_time'])
-             if 'chromedriver' in p.info['name']
-              if(datetime.datetime.now() - datetime.datetime.fromtimestamp(p.info['create_time'])).seconds //3600 >= 1])
+             if 'chrome' in p.info['name']
+              if(datetime.datetime.now() - datetime.datetime.fromtimestamp(p.info['create_time'])).seconds % 3600 / 60.0 >= 1])
 
 
 if __name__ == '__main__':
+    print(memProcessChromeDrive())
     instance_id = requests.get(_METADATAURL + '/instance-id').text
     instance_type = requests.get(_METADATAURL + '/instance-type').text
     dimensions = [{'Name': 'InstanceId', 'Value': instance_id}, {'Name': 'InstanceType', 'Value': instance_type}]
-    appendMetrics(currMetrics, dimensions, Name='ProcessChromedriver', Value=memProcessChromeDrive(), Unit='Value')
+    appendMetrics(currMetrics, dimensions, Name='ProcessChromedriver', Value=memProcessChromeDrive(), Unit='Percent')
     response = cw.put_metric_data(MetricData=currMetrics, Namespace='CustomMetricChromeDrive')
